a12c9fb3b7aa15e630b049b404bd2f44c1c52b88
lucas-salves <lucas.salves@terra.com>
2021-09-23 20:51:10 -0300

    add: creation of the method test

--- src/test/java/com/example/tdd/BookingServiceTest.java
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -7,15 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.tdd.service.BookingService;
+
+
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
 
 	@Autowired
-	BookinService bookingService;
+	BookingService bookingService;
 	
 	@Test
 	public void bookingTestServiceDaysCalculator() {
-		String name = "Lucas"
+		String name = "Lucas";
 		int days = bookingService.daysCalculatorWithDatabase(name);
 		
 		
